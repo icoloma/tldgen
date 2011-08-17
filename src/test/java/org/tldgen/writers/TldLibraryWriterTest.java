69fb8bef6c030a1d6d52d3f99059f1d18b58c98a
Ignacio Coloma <icoloma@extrema-sistemas.com>
2011-08-17 18:07:31 +0200

    Simplified test

--- src/test/java/org/tldgen/writers/TldLibraryWriterTest.java
+++ src/test/java/org/tldgen/writers/TldLibraryWriterTest.java
@@ -1,11 +1,11 @@
 package org.tldgen.writers;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.tldgen.factory.LibraryFactoryTest.OUTPUT_FOLDER;
 
-import java.io.FileReader;
+import java.io.File;
 
-import org.custommonkey.xmlunit.Diff;
+import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.tldgen.mock.MockLibraryFactory;
@@ -27,8 +27,8 @@ public class TldLibraryWriterTest {
 	public void writeTldTest() throws Exception {
 		TldLibraryWriter writer = new TldLibraryWriter();
 		writer.writeTLD(library, tldFolder);
-		FileReader expected = new FileReader( "src/test/resources/org/tldgen/writers/expected-output.tld");
-		Diff myDiff = new Diff( new FileReader(tldFolder + "/loom.tld"), expected);
-		assertTrue("XSL transformation worked as expected " + myDiff, myDiff.similar());
+		String expected = FileUtils.readFileToString(new File("src/test/resources/org/tldgen/writers/expected-output.tld"));
+		String actual = FileUtils.readFileToString(new File(tldFolder + "/loom.tld"));
+		assertEquals(expected, actual);
 	}
 }
